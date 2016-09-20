451fbab6261582f017ec366c455306e3792c308c
prpr2770 <prasanth.prahladan@colorado.edu>
2016-09-20 09:55:06 -0600

    1.5 works!

--- 1_5.py
+++ 1_5.py
@@ -23,6 +23,38 @@ def less_one_edit_away(a_str, b_str, num_prev_mismatches):
             return less_one_edit_away(a_str[1:], b_str[1:], num_prev_mismatches)
 
 
+def one_edit_away(a_str,b_str):
+    if abs(len(a_str) - len(b_str)) > 1:
+        return False
+    elif len(a_str) > len(b_str):
+        one_insert_away(a_str,b_str)
+    elif len(a_str) < len(b_str):
+         one_insert_away(b_str,a_str)
+    else:
+         one_replace_away(a_str,b_str)
+
+def one_replace_away(a_str,b_str):
+    diff_count = 0
+    for a_chr, b_chr in zip(a_str,b_str):
+        if a_chr != b_chr:
+            diff_count = diff_count + 1
+            if diff_count > 1:
+                return False
+    return True
+
+def one_insert_away(a_str,b_str):
+    diff_count = 0
+    for idx in xrange(0,len(a_str)):
+        c_str = a[:idx] + a[idx+1:]
+        if c_str == b_str:
+            diff_count = diff_count + 1
+
+    if diff_count > 0:
+        return True
+    else:
+        return False
+
+
 
 if __name__=="__main__":
     a_str = raw_input("Enter string with spaces\n")
